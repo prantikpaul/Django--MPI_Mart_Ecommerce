4c6cad0c84f567276b9670fe7110b700339aabfd
yourusername <28738988+prantikpaul@users.noreply.github.com>
2023-11-20 04:41:50 +0600

    added products for categorywize product view

--- Products/urls.py
+++ Products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import *
 
 
 urlpatterns = [
+    path('products/<int:id>',all_prod,name='all_prod'),
 
 ]
--- Products/views.py
+++ Products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import product
 
 # Create your views here.
+
+def all_prod(request,id):
+    show_prod=product.objects.filter(category=id)
+    
+
+
+    return render(request,'product/all_prod.html',locals())
