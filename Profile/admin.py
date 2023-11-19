86f3cec4aae5e5e0c3603a3664cafa632800c1a2
yourusername <28738988+prantikpaul@users.noreply.github.com>
2023-11-20 02:56:35 +0600

    added product and category database

--- Profile/admin.py
+++ Profile/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(category)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('id','name','price','category','quantity','featured_prod','New_arrivals','Best_selling_prod') 
+    list_editable=('price','quantity','featured_prod','New_arrivals','Best_selling_prod')
+admin.site.register(product,ProductAdmin)
--- Profile/models.py
+++ Profile/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class category(models.Model):
+    
+    name=models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+    
+class product(models.Model):
+    name=models.CharField(max_length=200)
+    price=models.IntegerField()
+    short_details=models.TextField()
+    quantity=models.PositiveIntegerField()
+    prod_img=models.ImageField(upload_to='product_img/')
+    sub_prod_img1=models.ImageField(upload_to='product_img/sub_prod_img/')
+    sub_prod_img2=models.ImageField(upload_to='product_img/sub_prod_img/')
+    sub_prod_img3=models.ImageField(upload_to='product_img/sub_prod_img/')
+    description=models.TextField()
+    category=models.ForeignKey(category,on_delete=models.CASCADE)
+    featured_prod=models.BooleanField(default=False)
+    New_arrivals=models.BooleanField(default=False)
+    Best_selling_prod=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
