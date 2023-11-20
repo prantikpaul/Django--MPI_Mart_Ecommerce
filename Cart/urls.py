15daa9a1dc732fb768e8b2e09d70a1d86fe94789
yourusername <28738988+prantikpaul@users.noreply.github.com>
2023-11-21 04:53:39 +0600

    fixed add to cart and checkout qunatity

--- Cart/urls.py
+++ Cart/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('add_to_cart/<int:id>/',add_to_cart,name='add_to_cart'),
     path('view_cart/',view_cart,name='view_cart'),
     path('empty_cart/',empty_cart,name='empty_cart'),
+    path('cart_clear/<id>/',cart_clear,name='cart_clear'),
+    path('update_cart_qnty/<int:id>/<str:action>/',update_cart_qnty,name='update_cart_qnty'),
 
 ]
--- Cart/views.py
+++ Cart/views.py
@@ -9,7 +9,6 @@ from django.contrib import messages
 
 def add_to_cart(request,id):
     prod=product.objects.get(pk=id)
-
     try:  
         ppp= cart.objects.filter(user=request.user, product=prod).first()
         if ppp:
@@ -18,9 +17,11 @@ def add_to_cart(request,id):
                     get_quty=request.POST['quantity']
                     ppp.quantity+=int(get_quty)
                     ppp.save()
+                else:
+                    ppp.quantity+=1
+                    ppp.save()
             except:
-                ppp.quantity+=1
-                ppp.save()
+                pass
 
         else: 
             add_cart=cart.objects.create(user=request.user , product=prod)
@@ -34,17 +35,33 @@ def add_to_cart(request,id):
 
 
 def view_cart(request):
-    all_prod=cart.objects.filter(user=request.user)
-    total_amnt=0
-    for i in all_prod :
-        total_amnt+=i.product.price*i.quantity
-    try:
-        if request.method=='POST':
-            ppp=request.POST['coupon_code']
-            print(ppp)
-    except:
-        pass
-    total_with_shipping=total_amnt+100
+    if request.user.is_authenticated:
+        
+        cart_len=cart.objects.filter(user=request.user)
+        prod_len_conunt=0 # collecting prod id if avaiable
+        for i in cart_len :
+            prod_len_conunt+=i.id # add prod id if avaiaable
+            
+        if prod_len_conunt>0: #if prod exisit in cart then show view cart page 
+         
+            all_prod=cart.objects.filter(user=request.user)
+            total_amnt=0
+            for i in all_prod :
+                total_amnt+=i.product.price*i.quantity
+            try:
+                if request.method=='POST':
+                    ppp=request.POST['coupon_code']
+                    print(ppp)
+            except:
+                pass
+            
+            total_with_shipping=total_amnt+100
+
+        else: #if prod don't exisit in cart then show empty cart page 
+            return redirect('empty_cart')
+
+    else:
+        return redirect ('index')
 
 
     
@@ -54,3 +71,31 @@ def view_cart(request):
 def empty_cart(request):
     
     return render(request,'cart/empty_cart.html')
+
+def cart_clear(request,id):
+  #cart er X button click korle product remove kore dibe ... 
+    ppq=cart.objects.filter(user=request.user , product=id)
+    for i in ppq:
+        i.delete()
+        
+
+    
+    return redirect (request.META['HTTP_REFERER'])
+
+def update_cart_qnty(request,id,action):
+    
+    prod = cart.objects.get(product=id, user=request.user) #cart er kon user kon prod er qun up korte chay ta get korbe
+    if action == 'plus': # jodi - button a chap dey
+        prod.quantity += 1
+
+    elif action == 'minus': # jodi + button a chap dey
+        if prod.quantity > 1:
+            prod.quantity -= 1
+
+        else:
+            prod.delete()
+            return redirect (request.META['HTTP_REFERER'])
+    prod.save()
+     
+    
+    return redirect (request.META['HTTP_REFERER'])
