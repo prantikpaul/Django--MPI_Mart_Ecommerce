7d2d75a4d7789fdb904644572285119ccfa7544c
yourusername <28738988+prantikpaul@users.noreply.github.com>
2023-11-22 04:25:12 +0600

    fixed index

--- Products/models.py
+++ Products/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class category(models.Model):
     
     name=models.CharField(max_length=150)
+    category_pic=models.ImageField(upload_to='product_img/category_img/')
 
     def __str__(self):
         return self.name
