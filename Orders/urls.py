c858039694d8be8606fbb1dd75f92753e13179e4
yourusername <28738988+prantikpaul@users.noreply.github.com>
2023-11-24 03:46:12 +0600

    added checkout page and some feature

--- Orders/urls.py
+++ Orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import *
 
 
 urlpatterns = [
+    path('checkout/',checkout,name='checkout'),
 
 ]
--- Orders/views.py
+++ Orders/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def checkout(request):
+    
+
+    return render (request,'order/checkout.html',locals())
