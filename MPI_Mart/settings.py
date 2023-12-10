e46af76aee2bb597b9017d5322a1cfd3d4b9ca66
yourusername <28738988+prantikpaul@users.noreply.github.com>
2023-12-11 03:59:58 +0600

    Add your own mail and app pass

--- MPI_Mart/settings.py
+++ MPI_Mart/settings.py
@@ -149,5 +149,5 @@ EMAIL_BackEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "assassins81007@gmail.com"
-EMAIL_HOST_PASSWORD = "ikwrqssevfzgqttq"
+EMAIL_HOST_USER = " "
+EMAIL_HOST_PASSWORD = ""
